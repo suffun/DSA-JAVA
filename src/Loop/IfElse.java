c1480b918591dfe9ce4f81479c8946d1ede596a6
sufiyan khan <suffunew@gmail.com>
2026-01-23 21:43:29 +0530

    practice if else

--- src/Loop/IfElse.java
+++ src/Loop/IfElse.java
@@ -1,4 +1,14 @@
 package Loop;
-
+import java.util.Scanner;
 public class IfElse {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n%2==0){
+            System.out.println(n +" : is even number");
+        }
+        else{
+            System.out.println(n +" : is Odd number");
+        }
+    }
 }
