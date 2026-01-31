31ea4f3f39f3b08b804506efc27f0163393c3b84
sufiyan khan <suffunew@gmail.com>
2026-01-31 19:57:20 +0530

    AlphanumericTriangle

--- src/Pattern_Printing/AlphanumericTriangle.java
+++ src/Pattern_Printing/AlphanumericTriangle.java
@@ -1,4 +1,20 @@
 package Pattern_Printing;
 
+import java.util.Scanner;
+
 public class AlphanumericTriangle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+               if(i%2!=0){
+                   System.out.print(j+ " ");
+               }else{
+                   System.out.print((char)(j+64)+ " ");
+               }
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ src/Pattern_Printing/FloydsTriangle.java
@@ -0,0 +1,4 @@
+package Pattern_Printing;
+
+public class FloydsTriangle {
+}
