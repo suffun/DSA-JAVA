331bc5fbfcf813ff3941ecbe3b560a0f75bee02f
sufiyan khan <suffunew@gmail.com>
2026-01-31 19:58:38 +0530

    hollow rectangle

--- src/Pattern_Printing/HollowRectangle.java
+++ src/Pattern_Printing/HollowRectangle.java
@@ -2,15 +2,37 @@ package Pattern_Printing;
 
 import java.util.Scanner;
 
+//public class HollowRectangle {
+//    static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int m = sc.nextInt();
+//        int n = sc.nextInt();
+//
+//        for(int i =1; i<=m; i++){
+//            for(int j = 1; j<=n; j++){
+//                if(i==1 || i==m || j==1 || j==n){
+//                    System.out.print("*");
+//                }else{
+//                    System.out.print(" ");
+//                }
+//            }
+//
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+
+
 public class HollowRectangle {
     static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
         int n = sc.nextInt();
-
-        for(int i =1; i<=m; i++){
+        int mid = n/2 +1;
+        for(int i =1; i<=n; i++){
             for(int j = 1; j<=n; j++){
-                if(i==1 || i==m || j==1 || j==n){
+                if(i==mid ||  j==mid){
                     System.out.print("*");
                 }else{
                     System.out.print(" ");
