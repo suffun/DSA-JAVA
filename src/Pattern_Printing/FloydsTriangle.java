73fc0d96bf3940cd96874e9dc9f596449067cb11
sufiyan khan <suffunew@gmail.com>
2026-01-31 19:58:11 +0530

    floyd triangle

--- src/Pattern_Printing/FloydsTriangle.java
+++ src/Pattern_Printing/FloydsTriangle.java
@@ -1,4 +1,20 @@
 package Pattern_Printing;
 
+import java.util.Scanner;
+
 public class FloydsTriangle {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 1;
+        int mid = n/2 +1;
+        for(int i =1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(a+" ");
+                a++;
+            }
+
+            System.out.println();
+        }
+    }
 }
