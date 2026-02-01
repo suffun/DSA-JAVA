e11218a7bd3cb2b6076c01025b8589cac2b03a8f
sufiyan khan <suffunew@gmail.com>
2026-02-01 22:58:17 +0530

    src/Pattern_Printing/TriangleVerticallyFlipped.java

---
+++ src/Pattern_Printing/TriangleVerticallyFlipped.java
@@ -0,0 +1,37 @@
+package Pattern_Printing;
+
+import java.util.Scanner;
+//
+//public class TriangleVerticallyFlipped {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for(int i =1; i<=n; i++){
+//            for(int j=1; j<=(n-i); j++){
+//                System.out.print("  ");
+//            }
+//            for(int j =1; j<=i; j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
+
+public class TriangleVerticallyFlipped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i =1; i<=n; i++){
+            for(int j=1; j<=(n-i); j++){
+                System.out.print("  ");
+            }
+            for(int j =1; j<=i; j++){
+                System.out.print((char)(i+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
