37b77d07dec688d167ec6e839567d43490407214
sufiyan khan <suffunew@gmail.com>
2026-02-01 23:25:10 +0530

    tflipped

--- src/Pattern_Printing/TriangleVerticallyFlipped.java
+++ src/Pattern_Printing/TriangleVerticallyFlipped.java
@@ -20,17 +20,41 @@ import java.util.Scanner;
 
 
 
+//public class TriangleVerticallyFlipped {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for(int i =1; i<=n; i++){
+//            for(int j=1; j<=(n-i); j++){
+//                System.out.print("  ");
+//            }
+//            for(int j =1; j<=i; j++){
+//                System.out.print((char)(i+64)+" ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+
 public class TriangleVerticallyFlipped {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
         int n = sc.nextInt();
-        for(int i =1; i<=n; i++){
-            for(int j=1; j<=(n-i); j++){
+
+        for (int i = 1; i <= n; i++) {
+            // 1. Loop for leading spaces (increases)
+            for (int j = 1; j < i; j++) {
                 System.out.print("  ");
             }
-            for(int j =1; j<=i; j++){
-                System.out.print((char)(i+64)+" ");
+
+            // 2. Loop for stars (decreases)
+            for (int j = 1; j <= (n - i + 1); j++) {
+                System.out.print("* ");
             }
+
+            // Move to the next line
             System.out.println();
         }
     }
