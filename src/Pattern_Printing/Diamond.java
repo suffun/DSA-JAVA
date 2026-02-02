9be66867c641cf3b733f3449053fe09f8588c345
sufiyan khan <suffunew@gmail.com>
2026-02-02 18:37:08 +0530

    diamond

--- src/Pattern_Printing/Diamond.java
+++ src/Pattern_Printing/Diamond.java
@@ -7,19 +7,35 @@ public class Diamond {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n: ");
         int n = sc.nextInt();
-        int a = 1;
+        int nsp =n - 1;
+        int nst = 1;
         for (int i = 1; i <= n; i++) {
-            // 1. Loop for leading spaces (increases)
-            for (int j = 1; j <= n-i; j++) {
+            for (int j = 1; j <= nsp; j++) {
                 System.out.print("  ");
             }
 
-            // 2. Loop for stars increase
-
-            for (int j = 1; j <= (2*i-1); j++) {
+            for (int j = 1; j <= nst; j++) {
                 System.out.print("* ");
-                a+=2;
             }
+            nsp--;
+            nst+=2;
+
+            // Move to the next line
+            System.out.println();
+        }
+        nsp =1;
+        nst = 2*n-3;
+
+        for (int i = 1; i <= n-1; i++) {
+            for (int j = 1; j <= nsp; j++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 1; j <= nst; j++) {
+                System.out.print("* ");
+            }
+            nsp++;
+            nst-=2;
 
             // Move to the next line
             System.out.println();
