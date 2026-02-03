74b285ced4623ff0a5ea75a9cfafb4fe7a6f698b
sufiyan khan <suffunew@gmail.com>
2026-02-03 23:47:07 +0530

    power

--- src/Methods/power.java
+++ src/Methods/power.java
@@ -1,5 +1,5 @@
-package Methods;
-import java.util.Scanner;
+//package Methods;
+//import java.util.Scanner;
 //public class power {
 //    public static void main(String[] args) {
 //        Scanner sc = new Scanner(System.in);
@@ -10,7 +10,8 @@ import java.util.Scanner;
 //
 //    }
 //}
-
+package Methods;
+import java.util.Scanner;
 public class power {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
