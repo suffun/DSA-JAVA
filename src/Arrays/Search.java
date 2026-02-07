a373f361ca594a35f307a292f00953beac3c5703
sufiyan khan <suffunew@gmail.com>
2026-02-07 14:26:52 +0530

    search in array

--- src/Arrays/Search.java
+++ src/Arrays/Search.java
@@ -1,21 +1,14 @@
 package Arrays;
 
+import java.util.Scanner;
+
 public class Search {
-    public static void main(String[] args) { // Fixed: Added 'public static'
-        int[] arr = {2, 4, 4, 5, 77, 8, 89, 89, 9, 96, 65, 55, 5};
-        int target = 8;
-        boolean found = false; // The flag
+    static void main(String[] args) {
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == target) {
-                System.out.println("Yo! We found your number " + target + " at index " + i);
-                found = true;
-                break; // Stop searching once found
-            }
-        }
-
-        if (!found) {
-            System.out.println("Sorry, we did not find the number " + target);
+        int[] arr = {2,4,4,5,77,8,89,89,9,96,65,55,5};
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] ==8) System.out.println("yo we found your number "+ arr[i]);
+            else System.out.println("Sorry we do not found");
         }
     }
 }
