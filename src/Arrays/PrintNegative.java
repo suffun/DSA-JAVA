dc8666d5c7af0523c3a0f0d7e048442dc42518aa
sufiyan khan <suffunew@gmail.com>
2026-02-07 14:26:17 +0530

    print negative

--- src/Arrays/PrintNegative.java
+++ src/Arrays/PrintNegative.java
@@ -1,4 +1,17 @@
 package Arrays;
-
+import java.util.Scanner;
 public class PrintNegative {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.print("Enter array elements :");
+        for(int i=0; i<=n-1; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0; i<=n-1; i++){
+            if(arr[i]<0) System.out.print(arr[i]+" ");
+        }
+    }
 }
