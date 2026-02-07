80e2d5ccd143d8bd2f866f0908bf2c6f632e9dff
sufiyan khan <suffunew@gmail.com>
2026-02-07 14:25:24 +0530

    for each loop

---
+++ src/Arrays/ForEach.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int [] arr = {44,55,54,3,66,4,4};
+        for(int ele : arr){
+            System.out.println(ele+" ");
+        }
+    }
+}
