b0cb27f0cff04fb2d0277aa6d9fd801be7ab79e3
sufiyan khan <suffunew@gmail.com>
2026-02-07 14:27:36 +0530

    find max element in array

---
+++ src/Arrays/MaxElement.java
@@ -0,0 +1,57 @@
+package Arrays;
+
+import java.util.Scanner;
+//public class MaxElement {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+
+/// /        int n = sc.nextInt();
+//        int[] arr = {1, 9, 4, 5, 6, 7, 8, 8, 3, 3, 311};
+//        int n = arr.length;
+//        int max = arr[0];
+//        for (int i = 0; i < n; i++) {
+//                if(arr[i]>max) max = arr[i];
+//        }
+//
+//        System.out.println(max);
+//
+//
+//    }
+//}
+
+//print the minium no of array
+
+//public class MaxElement {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int[] arr = {100, 9, 4, 5, 6, 7, 8, 8, 3, 3, 1};
+//        int n = arr.length;
+//        int min = Integer.MAX_VALUE;
+//        for (int i = 0; i < n; i++) {
+//            if (arr[i] < min) min = arr[i];
+//        }
+//
+//        System.out.println(min);
+//
+//
+//    }
+//}
+
+
+//public class MaxElement {
+//    static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.print("Enter array size : ");
+//        int n = sc.nextInt();
+//        int[] arr = new int[n];
+//        System.out.print("Enter array elements :");
+//        for(int i=0; i<=n-1; i++){
+//            arr[i] = sc.nextInt();
+//        }
+//        int max = Integer.MIN_VALUE;
+//        for(int i=0; i<=n-1; i++){
+//           if(max<arr[i]) max=arr[i];
+//        }
+//        System.out.println(max);
+//    }
+//}
