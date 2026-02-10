2dcf06fae446c9f6a4abfc9f140bce34e8512fd3
sufiyan khan <suffunew@gmail.com>
2026-02-10 21:16:13 +0530

    optimized bubble sort

--- src/Basic_Sorting/BubbleSort.java
+++ src/Basic_Sorting/BubbleSort.java
@@ -12,15 +12,19 @@ public class BubbleSort {
         int[] arr = {5,-2,6,7,2,0,7,2};
         int n = arr.length;
         print(arr);
+        int swap = 0;
         for(int i =0; i<n-1; i++){
             for(int j=0; j<n-1-i; j++){
                 if(arr[j]>arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+                    swap++;
                 }
             }
+            if(swap==0) break;
         }
+
         print(arr);
 
     }
