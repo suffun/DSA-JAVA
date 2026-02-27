51a69ac41e9d31c56d63683e97c589b88a078f2b
sufiyan khan <suffunew@gmail.com>
2026-02-27 22:18:55 +0530

    factorial

--- src/Loop/factorial.java
+++ src/Loop/factorial.java
@@ -12,19 +12,19 @@
 //    }
 //}
 
-//package Loop;
-//import java.util.Scanner;
-//
-//public class factorial {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//
-//        int fact = 1;
-//        for(int i = 1; i <= n; i++) {
-//            fact *= i;   // multiply step by step
-//            System.out.println(i + "! = " + fact);
-//        }
-//    }
-//}
+package Loop;
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int fact = 1;
+        for(int i = 1; i <= n; i++) {
+            fact *= i;   // multiply step by step
+            System.out.println(i + "! = " + fact);
+        }
+    }
+}
 
