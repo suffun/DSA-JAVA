ebaccb6492d46eb15a6a278a9849164eaa9b3ce6
sufiyan khan <suffunew@gmail.com>
2026-05-20 13:09:46 +0530

    three methods to print 1 to n

--- src/Recursion/OneToN.java
+++ src/Recursion/OneToN.java
@@ -2,16 +2,48 @@ package Recursion;
 
 import java.util.Scanner;
 public class OneToN {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        print(1,n);
+//    }
+//    public static void print(int x, int n) {
+//        if(x>n) return;
+//       System.out.println(x);
+//        print(x+1,n);
+//
+//
+//
+//    }
+//    SECOND WAY OF PRINTING
+//    static int n;
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        n = sc.nextInt();
+//        print(1);
+//    }
+//    public static void print(int x) {
+//        if(x>n) return;
+//        System.out.println(x);
+//        print(x+1);
+//
+//
+//
+//    }
+
+//    THIRD WAY OF PRINTING
+
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        print(1,n);
+        print(n);
     }
-    public static void print(int x, int n) {
-        if(x>n) return;
-       System.out.println(x);
-        print(x+1,n);
+    public static void print(int n) {
+        if(n==0) return;
 
+        print(n-1);
+        System.out.println(n);
 
 
     }
