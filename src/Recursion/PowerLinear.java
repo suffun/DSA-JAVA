a1d9c61d49d04dcfea14197d74df62a1ca9e3ea6
sufiyan khan <suffunew@gmail.com>
2026-05-20 17:04:58 +0530

    update in more efficient way

--- src/Recursion/PowerLinear.java
+++ src/Recursion/PowerLinear.java
@@ -15,7 +15,10 @@ public class PowerLinear {
     }
     public static int pow(int a, int b) {
         if(b==0) return 1;
-        return a*pow(a,b-1);
+        int call = pow(a,b/2);
+        if(b%2==0) return call*call;
+        else return a*call*call;
+
 
     }
 }
