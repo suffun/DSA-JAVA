9350bd8a4bb3c28525215396e66a2d6ad5967a7e
sufiyan khan <suffunew@gmail.com>
2026-06-10 17:48:45 +0530

    feat: add passing objects to methods example

--- src/OOPS/PassingClassesToMethods.java
+++ src/OOPS/PassingClassesToMethods.java
@@ -1,4 +1,47 @@
 package OOPS;
 
 public class PassingClassesToMethods {
+    public static class Product{
+        String name;
+        int price;
+        void print(){
+            System.out.println(name+" "+ price);
+        }
+
+    }
+    private static void change(Product x){
+        x.price= 900;
+
+
+    }
+
+    public static void main(String[] args) {
+        Product c = new Product();
+        c.name = "Handwash";
+        c.price = 300;
+
+        Product d = new Product();
+        d.name = "Sunglasses";
+        d.price = 7700;
+
+        Product e = new Product();
+        e.name = "earbuds";
+        e.price = 10000;
+
+
+
+        c.print();
+        d.print();
+        e.print();
+
+        change(c);
+
+
+        System.out.println(c.price);
+
+
+    }
+
+
+
 }
